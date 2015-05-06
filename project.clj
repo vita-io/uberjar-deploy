16e2a2757cbad607092448d8557cb7157831b75f
Ruben Weijers <ruben@vita.io>
2015-05-06 16:03:01 +0200

    bump version to 1.0.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject brightnorth/uberjar-deploy "1.0.1"
+(defproject vita-io/uberjar-deploy "1.0.2"
 
-  :description "A plugin that deploys an uberjar instead of a jar.  Intended to be used as an alias with the lein-release plugin."
+  :description "A plugin that deploys an uberjar instead of a jar.  Intended to be used as an alias with the lein-release plugin. Forked off https://github.com/BrightNorth/uberjar-deploy"
 
-  :url "https://github.com/BrightNorth/uberjar-deploy"
+  :url "https://github.com/vita-io/uberjar-deploy"
 
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
