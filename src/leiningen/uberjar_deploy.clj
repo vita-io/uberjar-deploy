36350b4996f5632ae0009bf23bab01a3f17f66db
Ruben Weijers <ruben@vita.io>
2015-05-06 16:02:49 +0200

    add check if cur-branch is in `:deploy-branches`

--- src/leiningen/uberjar_deploy.clj
+++ src/leiningen/uberjar_deploy.clj
@@ -3,17 +3,41 @@
     [leiningen.core.main :as main :refer [info]]
     [leiningen.deploy :as deploy :refer [deploy]]
     [leiningen.jar :as jar :refer [get-jar-filename]]
-    [leiningen.pom :as pom :refer [snapshot?]]))
+    [leiningen.pom :as pom :refer [snapshot?]]
+    [clojure.java.shell :as sh]))
 
-(defn uberjar-deploy
-  "Deploys an existing uberjar with lein deploy"
-  [project & args]
-  (main/info "Preparing to deploy uberjar")
-  (let [repository (if (pom/snapshot? project) "snapshots" "releases")
-        group (:group project)
-        name (:name project)
-        identifier (format "%s/%s" group name)
-        version (:version project)
-        file (or (:jar-filename project) (jar/get-jar-filename project :standalone))]
-    (main/info "Running: lein deploy" repository identifier version file)
-    (deploy/deploy project repository identifier version file)))
+(defmacro with-private-fns [[ns fns] & code]
+  "Refers private fns from ns and runs code in context."
+  `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
+     ~@code))
+
+
+(defn cur-branch []
+  (-> (sh/sh "git" "rev-parse" "--abbrev-ref" "HEAD")
+      :out
+      butlast
+      clojure.string/join))
+
+
+(with-private-fns [leiningen.deploy [in-branches]]
+  (defn uberjar-deploy
+    "Deploys an existing uberjar with lein deploy"
+    [project & args]
+    (main/info "Preparing to deploy uberjar")
+
+    (let [branches (set (:deploy-branches project))]
+       (when (and (seq branches)
+                  (in-branches branches))
+         (apply main/abort "Can only deploy from branches listed in"
+                ":deploy-branches:" branches
+                "; current branch: " (cur-branch))))
+
+    (let [repository (if (pom/snapshot? project) "snapshots" "releases")
+          group (:group project)
+          name (:name project)
+          identifier (format "%s/%s" group name)
+          version (:version project)
+          file (or (:jar-filename project) (jar/get-jar-filename project :standalone))]
+      (main/info "Running: lein deploy" repository identifier version file)
+      (deploy/deploy project repository identifier version file)))
+)
